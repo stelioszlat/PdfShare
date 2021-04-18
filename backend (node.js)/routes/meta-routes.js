9fe579dde759f1f01634edcd9619bc3cf3cd94fc
stelioszlat <steliosdj6@gmail.com>
2021-04-18 20:34:36 +0300

    removed backend (node.js) directory

--- backend (node.js)/routes/meta-routes.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const metaController = require('../controllers/meta-controller');
-
-const router = express.Router();
-
-router.post('/metadata', metaController.addMetadata);
