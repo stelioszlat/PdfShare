80829bb927da5c35d394ef2ace504a9a53a0356a
stelioszlat <steliosdj6@gmail.com>
2021-04-18 19:22:43 +0300

    added argparse functionalities and connected

--- main.py
+++ main.py
@@ -1,31 +1,40 @@
-import argparse as arg
+from argparse import ArgumentParser
 from extract import *
-import pymongo
+import os
 
 if __name__=='__main__':
 
     # main script
-    change_dir()
+    parser = ArgumentParser()
 
-    # parser = arg.ArgumentParser()
-    # parser.add_argument('-h', '--help', help='usage instructions')
-    # parser.add_argument('-a', '--advanced', help='custom keywords, biased parsing...etc')
-    # parser.add_argument('-m', '--multiple', help='add multiple files at once')
-    # parser.add_argument('file')
+    subparsers = parser.add_subparsers()
 
-    # fd = parser.file
-    # pdf_reader = open_pdf(fd)
-    pdf_reader = open_pdf("demo1.pdf")
+    file_subparser = subparsers.add_parser('add')
 
-    # if parser.advanced:
-    #     keywords = input('Enter keywords (seperated by comma, can be updated later)')
-        
-    #     keywords.split(',').join(extract_meta_keywords(extract_meta(pdf_reader)))
+    file_mutex_group = file_subparser.add_mutually_exclusive_group()
+    file_group = file_subparser.add_argument_group()
+    file_group.add_argument('-k', '--keywords', nargs='+')
+    file_mutex_group.add_argument('-f', '--file', nargs=1, help="specify the file name (located in default folder")
+    file_mutex_group.add_argument('-a', '--all', action='store_true', help="extract all files from the default directory (not recommended)")
+    file_args = parser.parse_args()
+
+    if file_args.keywords:
+        keywords = args.keywords
+    else:
+        keywords = []
+
+    if file_args.file:
+        file_a = args.f[0]
+
+        dir = os.environ['HOME'] + '/Files/'
+        if file_a not in os.listdir(dir):
+            print('Error: file ' + file_a + ' not found in default folder')
+            exit()
+        else:
+            print('Extracting ' + file_a + ' from /Files')
+            result = extract(keywords, dir+file_a)
+            # print(result)
     
-    meta_keywords = extract_meta_keywords(extract_meta(pdf_reader))
-    user_keywords = ['cloud', 'computer', 'scale', 'data']
-
-
-    keywords = extract_data(pdf_reader, None)
-    
-    print(keywords)
+    if file_args.all:
+        result = extract_all(keywords)
+        # print(result)
