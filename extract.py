569c945b8ab3a92332af9959125d48b7b2821f79
stelioszlat <steliosdj6@gmail.com>
2021-04-18 17:24:22 +0300

    added extracting methods and tested them

--- extract.py
+++ extract.py
@@ -1,10 +1,12 @@
 # from threading import Thread
 from re import findall, IGNORECASE, ASCII
-
 from os import environ, chdir, mkdir, getcwd, listdir
 # from concurrent.futures import ThreadPoolExecutor
 
 from tika.parser import from_file
+
+import configparser as conf
+from collections import Counter
 
 mwd = getcwd()       # main working directory
 
@@ -20,11 +22,51 @@ def list_files(path=TEST_PATH):
         chdir(environ['HOME'] + '/Files')
         return list(filter(lambda f: f.endswith('.pdf'), files))
 
+def extract_full(file, min=4, max=17, most=20):
+    data = {}
+
+    text = from_file(file)['content']
+    ex = findall('\w+', text, flags=IGNORECASE)
+    filtered_ex = filter(lambda x: len(x) >= min and len(x) <= max, ex)
+    counted_ex = Counter(filtered_ex)
+    print(Counter(filtered_ex))
+    return dict(counted_ex.most_common(most))
+
+def extract_keywords(keywords, text):
+    data = {}
+    for k in keywords:
+        ex = findall(k, text, flags=IGNORECASE)
+        data[k] = len(ex)
+    return data
+
+def extract_all(keywords):
+    files = list_files()
+    data = {}
+
+    for f in files:
+        data = {
+            f: extract(keywords, f)
+        }
+
+    return data
+
+def extract(keywords, file):
+
+    parsed = from_file(file)
+    default_keywords = (parsed['metadata']['Keywords']).split(',')
+    # print(default_keywords.split(','))
+    extracted = extract_keywords(keywords, parsed['content'])
+    default = extract_keywords(default_keywords, parsed['content'])
+    return {
+        'metadata': parsed['metadata'],
+        'keywords': extracted,
+        'default': default
+    }
+
+    return data
+
 if __name__=='__main__':
 
-    files = list_files()
-    print(files)
-    # parsed = from_file(TEST_PATH+'demo1.pdf')
-
-    # print(parsed.keys())
-
+    # e = extract_all(['cloud', 'metadata', 'author', 'computing'])
+    # e = extract(['cloud', 'metadata', 'author'], TEST_PATH+'demo1.pdf')
+    # print(e)
