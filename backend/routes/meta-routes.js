8e47f48de008e4e06e69863e360d9ff450d21c92
stelioszlat <steliosdj6@gmail.com>
2021-04-26 23:04:15 +0300

    clean unckecked post request for metadata

--- backend/routes/meta-routes.js
+++ backend/routes/meta-routes.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const metaController = require('../controllers/meta-controller');
+const {check} = require('express-validator');
 
 const router = express.Router();
 
-router.post('/metadata', metaController.addMetadata);
+router.post('/new',
+    // [
+    //     check('fileName').notEmpty(),
+    //     check('uploader').notEmpty(),
+    //     check('keywords').isArray(),
+    // ],
+    metaController.addMetadata);
+
+
+// router.patch('/edit', metaController.updateMetadata);
+
+module.exports = router;
