d3f005ac8ce631c0c0ba1278efe1806f86fd5ecd
stelioszlat <steliosdj6@gmail.com>
2022-06-14 14:08:21 +0300

    user routes enhancements

--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -1,18 +1,17 @@
 const express = require('express');
 
-const User = require('../models/user');
-const authController = require('../controllers/auth-controller');
-const userController = require('../controllers/user-controller');
+const auth = require('../controllers/auth-controller');
+const user = require('../controllers/user-controller');
 const validate = require('../validation/userValidation');
 
-const authenticate = authController.authenticate;
+const authenticate = auth.authenticate;
 const router = express.Router();
 
 // /api/users
-router.get('/all', authenticate, userController.getUsers);
-router.post('/user/new', authenticate, userController.createUser);
-router.get('/user/:uid', authenticate, userController.getUserById);
-router.put('/user/:uid', authenticate, userController.updateUserById);
-router.delete('/user/:uid', authenticate, userController.deleteUserById);
+router.get('/all', authenticate, user.getUsers);
+router.post('/user/new', authenticate, user.createUser);
+router.get('/user/:uid', authenticate, user.getUserById);
+router.put('/user/:uid', authenticate, user.updateUserById);
+router.delete('/user/:uid', authenticate, user.deleteUserById);
 
 module.exports = router;
