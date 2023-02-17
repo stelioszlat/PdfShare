b5366493bfc66cd524e2d93fc0055be3d63baa21
Stelios Zlat <steliosdj6@gmail.com>
2023-02-17 23:58:27 +0200

    working on the admin page

---
+++ web/frontend/src/admin/UserEntryPanel.js
@@ -0,0 +1,163 @@
+import React, { useCallback, useEffect, useState } from 'react';
+
+import './UserEntryPanel.css';
+
+import Button from '../components/Button';
+import Error from '../error/Error';
+
+const UserEntryPanel = props => {
+    const [error, setError] = useState(null);
+    const [user, setUser] = useState(null);
+
+    const [enteredIsAdmin, setEnteredIsAdmin] = useState(false);
+    const [enteredEmail, setEnteredEmail] = useState('');
+    const [enteredUsername, setEnteredUsername] = useState('');
+    const [enteredPassword, setEnteredPassword] = useState('');
+    const [enteredConfirmPassword, setEnteredConfirmPassword] = useState('');
+    const [isValid, setIsValid] = useState(true);
+    const [validationMessage, setValidationMessage] = useState('');
+
+    useEffect(() => {
+        setUser(props.userEdited);
+    }, []);
+
+    const isAdminChangeHandler = event => {
+        setValidationMessage('');
+        console.log(event.target.value);
+        setEnteredIsAdmin(event.target.value);
+    }
+    const emailChangeHandler = event => {
+        setValidationMessage('');
+        setEnteredEmail(event.target.value);
+    }
+    const usernameChangeHandler = event => {
+        setValidationMessage('');
+        setEnteredUsername(event.target.value);
+    }
+    const passwordChangeHandler = event => {
+        setValidationMessage('');
+        setEnteredPassword(event.target.value);
+    }
+    const confirmPasswordChangeHandler = event => {
+        setValidationMessage('');
+        setEnteredConfirmPassword(event.target.value);
+    }
+
+    const createUserHandler = event => {
+        setIsValid(true);
+        setValidationMessage('');
+
+        if (enteredUsername.trim() === '') {
+            setValidationMessage('Please enter a username');
+            setIsValid(false);
+            return;
+        }
+        if (enteredEmail.trim() === '') {
+            setValidationMessage('Please enter an email');
+            setIsValid(false);
+            return;
+        }
+        if (enteredPassword.trim() === '') {
+            setValidationMessage('Please enter a password');
+            setIsValid(false);
+            return;
+        }
+        if (enteredConfirmPassword.trim() === '') {
+            setValidationMessage('Please confirm your password');
+            return;
+        }
+        setEnteredIsAdmin(false);
+        setEnteredEmail('');
+        setEnteredUsername('');
+        setEnteredPassword('');
+        setEnteredConfirmPassword('');
+        createUser();
+    }
+
+    const editUserHandler = event => {
+        setEnteredIsAdmin(enteredIsAdmin);
+        setEnteredEmail('');
+        setEnteredUsername('');
+        setEnteredPassword('');
+        setEnteredConfirmPassword('');
+        editUser();
+    }
+
+    const createUser = useCallback(async () => {
+        await fetch('http://127.0.0.1:8086/api/user', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: enteredUsername,
+                email: enteredEmail,
+                isAdmin: enteredIsAdmin,
+                password: enteredPassword,
+                rePassword: enteredConfirmPassword
+            })
+        }).then(response => {
+            if (!response.ok) {
+                return setError(response.json().then(data => {return data.message} ));
+            }
+            props.onDone();
+        }).catch(error => {
+            console.log(error);
+            setError(error);
+        });
+    });
+
+    const editUser = useCallback(async (userId) => {
+        await fetch('http://127.0.0.1:8086/api/user/' + userId, {
+            method: "PUT",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: user.username,
+                email: user.email,
+                isAdmin: user.isAdmin,
+            })
+        }).then(response => {
+            if (!response.ok) {
+                return setError(response.json().then(data => {return data.message} ));
+            }
+            props.onDone();
+        }).catch(error => {
+            console.log(error);
+            setError(error);
+        });
+    })
+
+    return (
+        <div className='new-user'>
+            <div>
+                <label>Admin</label>
+                <input name="admin" type="checkbox" onChange={isAdminChangeHandler}/>
+            </div>
+            <div>
+                <label>Username</label>
+                <input className="text-input" name="username" type="text" onChange={usernameChangeHandler}/>
+            </div>
+            <div>
+                <label>Email</label>
+                <input className="text-input" name="username" type="text" onChange={emailChangeHandler}/>
+            </div>
+            <div>
+                <label>Password</label>
+                <input className="text-input" name="username" type="password" onChange={passwordChangeHandler}/>
+            </div>   
+            <div>
+                <label>Confirm Password</label>
+                <input className="text-input" name="username" type="password" onChange={confirmPasswordChangeHandler}/>
+            </div>
+            {!props.editMode && <Button label="Create" onClick={createUserHandler}/>}
+            {props.editMode && <Button label="Edit" onClick={editUserHandler}/>}
+            {error ? <Error message={error.message} /> :
+                    !isValid ? <p className="validation-message">{validationMessage}</p> : <p></p>
+            }
+        </div>
+    );
+}
+
+export default UserEntryPanel;
---
+++ web/frontend/src/admin/UserRecords.js
@@ -0,0 +1,108 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import Button from '../components/Button';
+import IconButton from '../components/IconButton';
+
+import './UserRecords.css';
+
+const UserRecords = props => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetchUsers();
+    }, []);
+
+    const fetchUsers = useCallback(async () => {
+        await fetch('http://127.0.0.1:8086/api/user/all', {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(response => {
+            response.json().then(data => {
+                if (!data.users) {
+                    return setUsers([]);
+                }
+                data.users.sort((a, b) => { return a.username.localeCompare(b.username) });
+                setUsers(data.users);
+            }).catch(err => {
+                console.log(err);
+            })
+        })
+    })
+
+    const showEditUserHandler = (event, userId) => {
+        props.onEdit(userId);
+    }
+
+    const deleteUser = useCallback(async (userId) => {
+        await fetch('http://127.0.0.1:8086/api/user/' + userId, {
+            method: "DELETE",
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }).then(response => {
+            response.json().then(data => {
+                const filteredUsers = users.filter(user => {return user._id !== userId});
+                setUsers([...filteredUsers]);
+            })
+        }).catch(err => {
+            console.log(err);
+        })
+    })
+
+    const toggleUserActivation = useCallback(async (userId, active) => {
+        await fetch('http://127.0.0.1:8086/api/user/' + userId, {
+            method: "PUT",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                active: active
+            })
+        }).then(response => {
+            response.json().then(data => {
+                const activatedUser = users.find(user => {return user._id === userId});
+                activatedUser.active = active;
+                const filteredUsers = users.filter(user => {return user._id !== userId});
+                setUsers([...filteredUsers, activatedUser].sort((a, b) => { return a.username.localeCompare(b.username) }));
+            })
+        }).catch(err => {
+            console.log(err);
+        })
+    })
+
+    return (
+        <div className="user-records">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Admin</th>
+                        <th>Username</th>
+                        <th>Email</th>
+                        <th>Last Login</th>
+                        <th>Joined</th>
+                        <th>Last Modified</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map(user => {
+                        return <tr key={user._id}>
+                            <td>{user.isAdmin ? <b>Yes</b> : "No"}</td>
+                            <td>{user.username}</td>
+                            <td>{user.email}</td>
+                            <td>{user.lastLogin ? new Date(user.lastLogin).toLocaleString() : "Never" }</td>
+                            <td>{new Date(user.createdAt).toLocaleString()}</td>
+                            <td>{new Date(user.updatedAt).toLocaleString()}</td>
+                            {user.active && <td><Button label="Deactivate" onClick={() => toggleUserActivation(user._id, false)}/></td>}
+                            {!user.active && <td><Button label="Activate" onClick={() => toggleUserActivation(user._id, true)}/></td>}
+                            <td><IconButton src="ci_edit.png" onClick={() => showEditUserHandler(user._id)}/></td>
+                            <td><IconButton src="icomoon-free_bin.png" onClick={() => deleteUser(user._id)}/></td>
+                        </tr>
+                    })}
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default UserRecords;
