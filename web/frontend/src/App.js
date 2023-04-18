a8c59ad47cfaa95564394c767da2d074c72d30f0
Stelios Zlat <steliosdj6@gmail.com>
2023-04-18 13:57:17 +0300

    adding react router

--- web/frontend/src/App.js
+++ web/frontend/src/App.js
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { createBrowserRouter, redirect, RouterProvider } from 'react-router-dom';
+import React from 'react';
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
 
 import './App.css';
 
@@ -8,22 +7,57 @@ import AdminPage from './pages/AdminPage';
 import ErrorPage from './pages/ErrorPage';
 import LoginPage from './pages/LoginPage';
 import MainPage from './pages/MainPage';
+import FilePage from './pages/FilePage';
 import ProfilePage from './pages/ProfilePage';
+import UserPage from './pages/UserPage';
+import SignupPage from './pages/SignupPage';
+import ForgotPasswordPage from './pages/ForgotPasswordPage';
+import NewFilePage from './pages/NewFilePage';
+import ResultsPage from './pages/ResultsPage';
+
+const ProfileRoot = props => {
+  return (<><Outlet /></>)
+}
 
 const router = createBrowserRouter([
   {
-    path: '',
+    path: '/',
     element: <MainPage />,
-    errorElement: <ErrorPage />,
-    action: () => {
-      if (!localStorage.getItem('token')) {
-        return redirect("/login");
-      }
-    } 
+    errorElement: <ErrorPage />
   },
   {
-    path: "/login",
+    path: '/login',
     element: <LoginPage />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: '/signup',
+    element: <SignupPage />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: '/forgot-password',
+    element: <ForgotPasswordPage />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: '/home',
+    element: <MainPage />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: '/new-file',
+    element: <NewFilePage />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/file/:id",
+    element: <FilePage />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/results",
+    element: <ResultsPage />,
     errorElement: <ErrorPage />
   },
   {
@@ -33,23 +67,32 @@ const router = createBrowserRouter([
   },
   {
     path: "/profile",
-    element: <ProfilePage />,
-    errorElement: <ErrorPage />
+    element: <ProfileRoot />,
+    errorElement: <ErrorPage />,
+    children: [
+      {index: true, element: <ProfilePage />},
+      {path: ':userId', element: <UserPage />}
+    ]
+  },
+  {
+    path: "/error",
+    element: <ErrorPage />
   }
 ]);
 
 function App() {
-  const isAdmin = useSelector(state => state.auth.isAdmin);
-  const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
 
   return (
     <RouterProvider router={router}>
-      {isLoggedIn && isAdmin && <AdminPage />}
-      {!isLoggedIn && <LoginPage />}
-      {isLoggedIn && !isAdmin && <MainPage />}
-      {isLoggedIn && !isAdmin && <ProfilePage />}
+      <AdminPage />
+      <LoginPage />
+      <SignupPage />
+      <ForgotPasswordPage />
+      <MainPage />
+      <FilePage />
+      <ProfilePage />
+      <ResultsPage />
     </RouterProvider>
-    // <MainPage />
   );
 }
 
--- web/frontend/src/index.js
+++ web/frontend/src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 
 import { Provider } from 'react-redux';
 import store from './store/index';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 
 ReactDOM.render(
