9e4508f63b7fc286198cf1696292be31fd9fc499
stelioszlat <steliosdj6@gmail.com>
2026-06-05 15:42:13 +0300

    fixed file uploading for lambda functions

--- backend/serverless/handlers/upload-handler.js
+++ backend/serverless/handlers/upload-handler.js
@@ -1,5 +1,10 @@
 const awsS3 = require('@aws-sdk/client-s3');
 const dotenv = require('dotenv');
+const { createMetadata } = require('./metadata-handler');
+const parser = require('lambda-multipart-parser');
+const pdf = require('pdf-parse');
+const miner = require('text-miner');
+const path = require('path');
 
 dotenv.config();
 
@@ -11,61 +16,28 @@ const s3 = new awsS3.S3Client({
     region: 'eu-north-1',
 });
 
-const sanitizeFile = (file, cb) => {
+const isFileAllowed = (file) => {
     const fileExtensions = ['.pdf'];
-
-    const isAllowed = fileExtensions.includes(path.extname(file.originalname.toLowerCase()));
-    console.log("File is" + (isAllowed ? " " : "not ") + "allowed")
-
-    if (isAllowed) {
-        return cb(null, true);
-    } else {
-        return cb("File type not allowed!");
-    }
+    return fileExtensions.includes(path.extname(file.filename.toLowerCase()));
 }
 
-const uploader = multer({
-    storage: multer.diskStorage({
-        destination: (req, file, cb) => {
-          // Set the destination folder where files will be uploaded
-          cb(null, 'files/');
-        },
-        filename: (req, file, cb) => {
-          // Keep the original file name
-          cb(null, file.originalname);  // This will save the file with its original name
-        }
-      }),
-    fileFilter: (req, file, cb) => {
-        sanitizeFile(file, cb);
-    }
-});
+const extract = async (file) => {
 
-const extract = async (req, res, next) => {
-    const file = req.file;
-    console.log(file)
-    // const token = req.get('Authorization').split(' ')[0];
-
-    if (!file) {
-        return res.status(400).json({ message: 'File not found' });
-    }
-
+    const dataBuffer = file.content ? Buffer.from(file.content, 'base64') : null;
+    
     try {
-
-        const dataBuffer = fs.readFileSync('./files/' + file.originalname);
-
         if (!dataBuffer) {
-            return res.status(404).json({ message: 'File does not exist.' });
+            throw new Error('File does not exist.');
         }
 
         const data = await pdf(dataBuffer);
-
         data.text.replace(/^[_-\w]/gi, ' ');
 
-        console.log('Info ' + data.info);
-        console.log('Metadata ' + data.metadata);
-        console.log('Num pages ' + data.numpages);
-        console.log('Num render ' + data.numrender);
-        console.log('Version ' + data.version);
+        let cleanText = data.text.replace(/^[_-\w]/gi, ' ');
+        cleanText = cleanText.replace(/[\n\r\t\f\b]/g, " ");
+        cleanText = cleanText.replace(/[\x00-\x1F\x7F-\x9F]/g, "");
+        cleanText = cleanText.replace(/\s+/g, " ");
+        cleanText = cleanText.replace(/[^a-zA-Z ]/g, " ");
 
         const content = miner.Corpus(data.text);
         content.removeNewlines();
@@ -77,35 +49,70 @@ const extract = async (req, res, next) => {
         content.trim();
 
         const terms = new miner.DocumentTermMatrix(content);
+        const keywords = terms.findFreqTerms(100).filter(term => {
+            const isPureText = /^[a-zA-Z]+$/.test(term.word);
+    
+            return isPureText && term.word.length > 4;
+        });
 
-        const keywords = terms.findFreqTerms(100);
+        const savedFile = await createMetadata({ fileName: file.filename, uploader: 'unknown', keywords: keywords });
 
-        send({fileName: file, uploader: 'stelioszlat', keywords: keywords });
-
-        console.log('Sent file: ' + file);
+        if (!savedFile) {
+            console.error('Could not create metadata for file: ' + file.filename);
+            return false;
+        }
+        console.log('Sent file: ' + savedFile.fileName);
         
-        return res.status(200).json({ message: 'File sent.', fileName: file, keywords: keywords });
+        return savedFile;
     } catch (err) {
         console.error(err);
-        return res.status(409).json({ message: 'Bad Request' });
+        return false
     }
 };
 
-exports.handler = function (event, context, callback) {
-    const body = event.body;
+exports.upload = async function (event, context, callback) {
+    try {
+        const result = await parser.parse(event);
+        const uploadedFile = result.files.find(f => f.fieldname === 'file');
 
-    s3.putObject({
-        Bucket: process.env.S3_BUCKET,
-        Key: 'file_name',
-        Body: body,
-    }).then(response => {
-        callback(null, {
+        if (!uploadedFile) {
+            return callback(null, {
+                statusCode: 400,
+                body: JSON.stringify({ message: 'No file uploaded.' })
+            });
+        }
+        
+        if (!isFileAllowed(uploadedFile)) {
+            return callback(null, {
+                statusCode: 400,
+                body: JSON.stringify({ message: 'File type not allowed.' })
+            });
+        }
+
+        const file = await extract(uploadedFile);
+
+        if (!file) {
+            return callback(null, {
+                statusCode: 500,
+                body: JSON.stringify({ message: 'Could not process file.' })
+            })
+        }
+
+        await s3.putObject({
+            Bucket: process.env.S3_BUCKET,
+            Key: uploadedFile.filename,
+            Body: body,
+        });
+
+        return callback(null, {
             statusCode: 200,
-            body: {
-                message: 'File uploaded'
-            }
-        })
-    }).catch(err => {
-        console.log(err);
-    });
+            body: JSON.stringify({ message: 'File sent.' })
+        });
+    } catch (err) {
+        console.error(err);
+        return callback(null, {
+            statusCode: 500,
+            body: JSON.stringify({ message: 'Could not send file.' })
+        });
+    }
 }
